a86b20aa3da22344e9394dd3ed7ce86aa4e5b0a5
ComTech <mohibmubeen058@gmail.com>
2023-02-04 03:14:00 +0500

    first commmit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-function hello() {
-    var fullname = "Error!please enter a valid passward.";
-    alert(fullname)
-    var name = "Welcome To Js Land...\n Happy Coding!"
-    alert(name)
-    var num = "Welcome To Js Land..."
-    alert(num)
-    var land = "Hsppy Coding! \n ⬜Prevent this page from creating traditional diologs"
-    alert(land)
-    var well = "Hello...i can run js through in web browser"
-    alert(well)
-}
+// function hello() {
+//     var fullname = "Error!please enter a valid passward.";
+//     alert(fullname)
+//     var name = "Welcome To Js Land...\n Happy Coding!"
+//     alert(name)
+//     var num = "Welcome To Js Land..."
+//     alert(num)
+//     var land = "Hsppy Coding! \n ⬜Prevent this page from creating traditional diologs"
+//     alert(land)
+//     var well = "Hello...i can run js through in web browser"
+//     alert(well)
+// }
